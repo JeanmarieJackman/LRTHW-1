3ace1c537e01370d3b2b3d083248d393e94bcdfe
Brandon Walsh <bmw9t@virginia.edu>
2012-12-03 14:50:24 -0500

    Fizz Buzz, and others

---
+++ praxis_exercises/fizz_buzz.rb
@@ -0,0 +1,15 @@
+counter = 0
+while counter < 100
+	counter += 1
+	if ((counter % 3) == 0) && ((counter % 5) == 0)
+		puts "FizzBuzz"
+	elsif ((counter % 3) == 0)
+		puts "Fizz"
+	elsif ((counter % 5) == 0)
+		puts "Buzz"
+	else
+		puts counter
+	end
+end
+			
+									
