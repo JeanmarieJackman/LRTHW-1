1fa992512a8ced94701395ccedf924efc3655f3e
Brandon Walsh <bmw9t@virginia.edu>
2014-08-06 09:08:03 -0400

    First broken exercises.

---
+++ broken_exercises/branching.rb
@@ -0,0 +1,11 @@
+puts "Greetings! What is your year of origin?'
+origin == gets.chomp().to_i
+
+if origin < 1900
+	puts 'That's the past!'
+
+elseif origin > 1900 && origin < 2020
+	puts "That's the present!"
+
+elsif
+	puts "That's the future!"
