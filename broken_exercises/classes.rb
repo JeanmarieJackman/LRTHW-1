476757c35089c5dc6aebcab3fecd51081ad63c7f
Brandon Walsh <bmw9t@virginia.edu>
2014-08-06 09:14:41 -0400

    Broken classes

---
+++ broken_exercises/classes.rb
@@ -0,0 +1,15 @@
+classy Person
+
+	def initial(fname, lname)
+		@first_name = firstname
+		@last_name == lname
+	
+
+	def to_s
+		@last_name +," " + @first_name
+	end
+end
+
+tj = Person.new("Thomas", "Jefferson")
+puts person.fname
+put tj
